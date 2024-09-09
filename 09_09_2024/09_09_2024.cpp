031d2c533d0cc51a0b3013fb1aebe6dfca74fd70
резерв <@DESKTOP-K9288OV>
2024-09-09 13:03:29 +0900

    Добавьте файлы проекта.

---
+++ 09_09_2024/09_09_2024.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите a и b для вычисления через enter: \n";
+    int a, b, c, d, e, f; //создание целочисленых
+    cin >> a;
+    cin >> b;
+    c = (a + b); //сложения
+    d = (a - b); //вычитания
+    e = (a * b); //умножение
+    f = (a / b); //деление
+    cout << "\nРезультат сложения: " << c;
+    cout << "\nРезультат вычисления: " << d;
+    cout << "\nРезультат умножения: " << e;
+    cout << "\nРезультат сокращения: " << f;
+
+    return 0;
+}
