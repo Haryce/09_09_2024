33bca63b5465ef74e83d7a9e6c55fb4df97fbe92
Haryce <148765923+Haryce@users.noreply.github.com>
2024-09-30 14:41:49 +0900

    Add files via upload

---
+++ 09_09_2024/16_11sh.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath> //импортируем библиотеку cmath для математических вычислений
+
+using namespace std;
+
+int main() {
+	double x, y, z, a, b; //присваеваем переменным чтобы число после точки считалось
+
+	// Ввод значений x, y, z
+	cout << "Введите значение x: ";
+	cin >> x;
+	cout << "Введите значение y: ";
+	cin >> y;
+	cout << "Введите значение z: ";
+	cin >> z;
+
+	// Вычисление a
+	a = log(abs((y - sqrt(abs(x))) * (x - y / (z + x * x / 4))));
+
+	// Вычисление b
+	b = x - x * x / 6 + x * x * x * x * x / 120;
+
+	// Вывод результатов
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	return 0;
+}
