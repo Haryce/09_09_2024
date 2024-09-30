9bb450709470ddb13327d99a00ce2ef2c7ffe445
Haryce <148765923+Haryce@users.noreply.github.com>
2024-09-30 14:33:12 +0900

    Add files via upload

---
+++ 09_09_2024/16_31a.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	double a; //добавляем переменную чтобы число после точки считалось
+	setlocale(LC_ALL, "Russian"); 
+	cout << "Введите число a: "; //получаем данные от пользователя
+	cin >> a;
+
+	double zxc = a * a; // создал переменные "zxc" и "zx"  которые вычисляют формулу a^4 = (a^2) * (a^2)
+	double zx = zxc * zxc;
+
+	cout << "a^4 = " << zx << endl; //вывод
+
+	return 0;
+}
